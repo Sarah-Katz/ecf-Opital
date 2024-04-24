78c37d8eed165fd3283f8d111368ac67bd0ec420
Sarah-Katz <Sarahkatz59@gmail.com>
2024-04-24 19:58:06 +0200

    feat: Finished style, lacking search functionality

--- src/view/components/NavBar.jsx
+++ src/view/components/NavBar.jsx
@@ -1,26 +1,42 @@
-function NavBar() {
-    return (
-        <nav className='navbar' role='navigation' aria-label='main navigation'>
-            <div id='navbarBasicExample' className='navbar-menu'>
-                <div className='navbar-start'>
-                    <a className='navbar-item'>
-                        <span className='icon-text'>
-                            <span className='icon'>
-                                <i className='fas fa-address-card'></i>
-                            </span>
-                            <span>Patients</span>
-                        </span>
-                    </a>
+import { Link } from "react-router-dom";
 
-                    <a className='navbar-item'>
-                        <span className='icon-text'>
-                            <span className='icon is-large'>
-                                <i className='fas fa-square-h'></i>
-                            </span>
-                            <span>Services</span>
+function NavBar({ callback, search }) {
+    return (
+        <nav className='columns is-vcentered is-color-primary mb-1' role='navigation' aria-label='main navigation'>
+            <div className='column is-1 pt-3 pb-0 pl-5'>
+                <Link className='navbar-item' to={"/"}>
+                    <span className='icon-text has-text-white'>
+                        <span className='icon'>
+                            <i className='fas fa-xl fa-address-card'></i>
                         </span>
-                    </a>
-                </div>
+                        <span className='has-text-weight-semibold'>Patients</span>
+                    </span>
+                </Link>
+            </div>
+            <div className='column is-1 pt-3 pb-0'>
+                <Link className='navbar-item' to={"services"}>
+                    <span className='icon-text has-text-white'>
+                        <span className='icon'>
+                            <i className='fas fa-xl fa-square-h'></i>
+                        </span>
+                        <span className='has-text-weight-semibold'>Services</span>
+                    </span>
+                </Link>
+            </div>
+
+            <div className='field column is-6 is-offset-1'>
+                <p className='control has-icons-right pt-2'>
+                    <input
+                        className='input'
+                        type='text'
+                        placeholder='Entrez un nom de famille ou un numéro de sécurité social'
+                        onChange={callback}
+                        value={search}
+                    />
+                    <span className='icon is-small is-right'>
+                        <i className='fas fa-xl fa-magnifying-glass has-text-primary mt-3'></i>
+                    </span>
+                </p>
             </div>
         </nav>
     );
--- src/view/pages/Home.jsx
+++ src/view/pages/Home.jsx
@@ -1,7 +1,17 @@
-import NavBar from "../components/NavBar";
+import { useState } from 'react';
+import NavBar from '../components/NavBar';
 
 function Home() {
-    return <NavBar />;
+    const [search, setSearch] = useState("");
+    const handleChange = (e) => {
+        console.log(e.target.value);
+        setSearch(e.target.value);
+        setTimeout(() => {
+            //TODO: implement search
+            console.log("SEARCH");
+        }, 1000);
+    };
+    return <NavBar callback={handleChange} search={search}/>;
 }
 
 export default Home;
